6cfdbe516b844f26a4464abc2b2f7155d58df4fb
it24101502 <it24101502@my.sliit.lk>
2026-09-20 16:32:50 +0530

    Extend CI/CD pipeline for LeaveService

---
+++ database/leave/migrations/001_create_leave_database.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS Hostel_Leave_System
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+USE Hostel_Leave_System;
+
+-- add your leave_requests table etc. here, or in a 002_... file
